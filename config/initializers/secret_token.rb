f65bf9290af8e05808ecfc6512c09ebde6211c4d
Tony Deng <christony610@gmail.com>
2014-05-20 12:50:06 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TonysFaves::Application.config.secret_token = 'ecb953aa93badfdce5f340d3628871fe85943254bb5a04c99af0bf5e03cd0d5631819fa6a545d041e54c2e01055ddfd686a28c82532d2c3b5d368ac28ca086c0'
